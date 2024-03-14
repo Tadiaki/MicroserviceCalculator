18185d177089055f52e631ca60c67a5f551b1d93
Tadiaki <n0m1z@hotmail.com>
2024-03-14 13:31:56 +0100

    fixed merge conflicts

--- MicroserviceCalculator/CalculatorService/Entities/Result.cs
+++ MicroserviceCalculator/CalculatorService/Entities/Result.cs
@@ -15,18 +15,12 @@ namespace CalculatorService.Entities
             NumberTwo = numberTwo;
         }
 
-        // Constructor with parameters that EF Core can bind
-        public Result(double value, CalculationType type, string calculation)
-        {
-            Value = value;
-            Type = type;
-            Calculation = calculation;
-        }
-
-        public Result()
-        {
-        }
-
+        public int? Id { get; set; }
+        public double? Value { get; set; }
+        public CalculationType Type { get; set; }
+        public string Calculation { get; set; }
+        public double? NumberOne { get; set; }
+        public double? NumberTwo { get; set; }
 
     }
 }
