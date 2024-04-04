304976de1300cdf02fc017d8daff88eaf394d385
Tadiaki <n0m1z@hotmail.com>
2024-04-04 11:16:47 +0200

    d

--- MicroserviceCalculator/CalculatorService/Communications/Subscriptions.cs
+++ MicroserviceCalculator/CalculatorService/Communications/Subscriptions.cs
@@ -15,36 +15,36 @@ namespace CalculatorService.Communications
 
         public static void StartSubtractionSubscription(ResultService resultService)
         {
-            
+
             _resultService = resultService;
             var bus = RabbitHutch.CreateBus("host=rmq;port=5672;virtualHost=/;username=guest;password=guest");
-                var topic = "subtractionResult";
-                
+            var topic = "subtractionResult";
 
-                bus.PubSub.SubscribeAsync<CalculationResponseDTO>("CalcService-" + Environment.MachineName, e =>
+
+            bus.PubSub.SubscribeAsync<CalculationResponseDTO>("CalcService-" + Environment.MachineName, e =>
+            {
+                var propagator = new TraceContextPropagator();
+                var parentContext = propagator.Extract(default, e, (r, key) =>
                 {
-                    var propagator = new TraceContextPropagator();
-                    var parentContext = propagator.Extract(default, e, (r, key) =>
-                    {
-                        return new List<string>(new[] { r.Headers.ContainsKey(key) ? r.Headers[key].ToString() : String.Empty }!);
-                    });
-                    Baggage.Current = parentContext.Baggage;
+                    return new List<string>(new[] { r.Headers.ContainsKey(key) ? r.Headers[key].ToString() : String.Empty }!);
+                });
+                Baggage.Current = parentContext.Baggage;
 
-                    using var activity = Monitoring.ActivitySource.StartActivity("Received addition task", ActivityKind.Consumer, parentContext.ActivityContext);
-                    if (e != null)
-                    {
-                        _resultService.HandleCalculationResult(e);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Received null response from the message bus (subtractionResult).");
-                    }
-                    
-                }, x => x.WithTopic(topic));
-                
-                Monitoring.Log.Here()
-                    .Information(
-                        "Subscription set up in Calculation Service, ready to consume messages on route: {topic}", topic );
+                using var activity = Monitoring.ActivitySource.StartActivity("Received addition task", ActivityKind.Consumer, parentContext.ActivityContext);
+                if (e != null)
+                {
+                    _resultService.HandleCalculationResult(e);
+                }
+                else
+                {
+                    Console.WriteLine("Received null response from the message bus (subtractionResult).");
+                }
+
+            }, x => x.WithTopic(topic));
+
+            Monitoring.Log.Here()
+                .Information(
+                    "Subscription set up in Calculation Service, ready to consume messages on route: {topic}", topic);
         }
 
         public static void StartAdditionSubscription(ResultService resultService)
@@ -73,12 +73,12 @@ namespace CalculatorService.Communications
                 {
                     Console.WriteLine("Received null response from the message bus (additionResult).");
                 }
-                
+
             }, x => x.WithTopic(topic));
-            
+
             Monitoring.Log.Here()
                 .Information(
-                    "Subscription set up in Calculation Service, ready to consume messages on route: {topic}", topic );
+                    "Subscription set up in Calculation Service, ready to consume messages on route: {topic}", topic);
         }
     }
 }
